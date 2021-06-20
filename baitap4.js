eb8b9877faabb0efc568b1259635863ec59ddaf6
lamnguyen8998 <84464387+lamnguyen8998@users.noreply.github.com>
2021-06-20 15:35:41 +0700

    5 bai tap thuc hanh JS

---
+++ baitap4.js
@@ -0,0 +1,43 @@
+console.log("Bài tập 1");
+
+var ngaylamviec = 26;
+var luong1ngay = 100000;
+var congthuctinhluong = ngaylamviec * luong1ngay;
+console.log("Tiền lương làm việc 26 ngày:" ,congthuctinhluong);
+
+console.log("Bài tập 2")
+
+var a = 5;
+var b = 10;
+var c = 15;
+var d = 20;
+var e = 25;
+
+var giatritrungbinh = (a + b + c + d + e) / 5;
+console.log(giatritrungbinh);
+
+console.log("Bài tập 3")
+var tiendola = 2000;
+var giatienvietquyrado=23500;
+
+var tongsotiendolaquyratienviet = tiendola * giatienvietquyrado;
+console.log("Giá tiền 2000 USD quy đổi ra VND :", tongsotiendolaquyratienviet);
+
+console.log("Bài tập 4")
+var chieudai = 100;
+var chieurong = 40;
+
+var dientich = chieudai * chieurong;
+console.log("Diện tích hình chữ nhật:",dientich);
+
+var chuvihinhchunhat = (chieudai + chieurong) * 2;
+console.log("Chu vi hình chữ nhật:", chuvihinhchunhat);
+
+console.log("Bài tập 5");
+
+var a = 89;
+var so_hang_don_vi = a % 10;
+var so_hang_chuc = a / 10;
+
+var tong2kyso = Math.floor(so_hang_don_vi + so_hang_chuc);
+console.log("Tổng 2 ký số của số 89:", tong2kyso);
